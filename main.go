8583eb363192fde60d92d1debca3ced8afdd4622
ManiacMike <mikewantacareer@gmail.com>
2016-02-18 23:50:02 +0800

    add turing123 api

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+  "log"
+	"net/url"
+	"io/ioutil"
+)
+
+type ServiceError struct {
+	Msg string
+}
+
+func (e *ServiceError) Error() string {
+	return fmt.Sprintf("%s", e.Msg)
+}
+
+func Error(msg string) error {
+	return &ServiceError{msg}
+}
+
+func CheckErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+type ApiServer struct {
+	ApiName string
+}
+
+
+func (this *ApiServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+  msg := r.PostFormValue("msg")
+
+	switch this.ApiName {
+	case "message":
+		var reply string
+		fmt.Println(msg)
+		turingConfig, _ := getConfig("turing")
+		resp, err := http.PostForm(turingConfig["base_url"], url.Values{"uid":{"myself"}, "key": {turingConfig["key"]}, "info": {msg}})
+		if err != nil{
+			fmt.Println("request fail")
+			return
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil{
+			fmt.Println("request fail")
+			return
+		}
+		fmt.Println(string(body))
+		replyMap := JsonDecode(string(body))
+		if replyMap["code"] == 100000{
+			reply = replyMap["text"].(string)
+		}else if replyMap["code"] == 200000{
+			reply = "给你个链接接着  " + replyMap["url"].(string)
+		}
+		fmt.Fprint(w, reply)
+	default:
+		fmt.Fprint(w, "Invalid api")
+	}
+}
+
+func main(){
+  var err error
+
+  http.Handle("/message", &ApiServer{ApiName: "message"})
+
+	serverConfig, err := getConfig("server")
+	if err != nil {
+		log.Fatal("server config error:", err)
+	}
+
+	fmt.Println("listen on port " + serverConfig["port"])
+
+  if err = http.ListenAndServe(":"+serverConfig["port"], nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+
+}
---
+++ utils.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/bitly/go-simplejson" // for json get
+	"strconv"
+	"strings"
+	"time"
+	"github.com/larspensjo/config"
+)
+
+const LINE_SEPARATOR = "#LINE_SEPARATOR#"
+
+func JsonStrToMap(jsonStr string) map[string]interface{} {
+	jsonStr = strings.Replace(jsonStr, "\n", LINE_SEPARATOR, -1)
+	json, err := simplejson.NewJson([]byte(jsonStr))
+	if err != nil {
+		panic(err.Error())
+	}
+	var nodes = make(map[string]interface{})
+	nodes, _ = json.Map()
+	return nodes
+}
+
+func GenerateId() string {
+	return strconv.FormatInt(time.Now().UnixNano(), 10)
+}
+
+func JsonEncode(nodes interface{}) string {
+	body, err := json.Marshal(nodes)
+	if err != nil {
+		panic(err.Error())
+		return "[]"
+	}
+	return string(body)
+}
+
+func JsonDecode(jsonStr string) map[string]interface{} {
+	jsonStr = strings.Replace(jsonStr, "\n", LINE_SEPARATOR, -1)
+	var f interface{}
+	err := json.Unmarshal([]byte(jsonStr), &f)
+	if err != nil {
+		panic(err)
+	}
+	m := f.(map[string]interface{})
+	for k, v := range m {
+		switch v.(type) {
+		case string:
+			m[k] = v.(string)
+		case int:
+			m[k] = v.(int)
+		case float64:
+			m[k] = int(v.(float64))
+		}
+	}
+	return m
+}
+
+func getConfig(sec string) (map[string]string, error) {
+	targetConfig := make(map[string]string)
+	cfg, err := config.ReadDefault("config.ini")
+	if err != nil {
+		return targetConfig, Error("unable to open config file or wrong fomart")
+	}
+	sections := cfg.Sections()
+	if len(sections) == 0 {
+		return targetConfig, Error("no " + sec + " config")
+	}
+	for _, section := range sections {
+		if section != sec {
+			continue
+		}
+		sectionData, _ := cfg.SectionOptions(section)
+		for _, key := range sectionData {
+			value, err := cfg.String(section, key)
+			if err == nil {
+				targetConfig[key] = value
+			}
+		}
+		break
+	}
+	return targetConfig, nil
+}
