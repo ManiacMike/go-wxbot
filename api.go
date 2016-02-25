ced0711decd79df6ca395fce02608d35415f74b0
ManiacMike <mikewantacareer@gmail.com>
2016-02-25 14:18:41 +0800

    qrcode

---
+++ api.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"io/ioutil"
+	"strings"
+	"math/rand"
+	"time"
+)
+
+type ApiServer struct {
+	ApiName string
+}
+
+func randomEmoticon() string {
+    happy := []string{"_(┐「ε:)_","_(:3 」∠)_","(￣y▽￣)~*捂嘴偷笑",
+			"・゜・(PД`q｡)・゜・","(ง •̀_•́)ง","(•̀ᴗ•́)و ̑̑","ヽ(•̀ω•́ )ゝ","(,,• ₃ •,,)",
+			"(｡˘•ε•˘｡)","(=ﾟωﾟ)ﾉ","(○’ω’○)","(´・ω・`)","ヽ(･ω･｡)ﾉ","(。-`ω´-)",
+			"(´・ω・`)","(´・ω・)ﾉ","(ﾉ･ω･)","  (♥ó㉨ò)ﾉ♡","(ó㉨ò)","・㉨・","( ・◇・)？",
+			"ヽ(*´Д｀*)ﾉ","(´°̥̥̥̥̥̥̥̥ω°̥̥̥̥̥̥̥̥｀)","(╭￣3￣)╭♡","(☆ﾟ∀ﾟ)","⁄(⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄.",
+			"(´-ι_-｀)","ಠ౪ಠ","ಥ_ಥ","(/≥▽≤/)","ヾ(o◕∀◕)ﾉ ヾ(o◕∀◕)ﾉ ヾ(o◕∀◕)ﾉ","*★,°*:.☆\\(￣▽￣)/$:*.°★*",
+			"ヾ (o ° ω ° O ) ノ゙","╰(*°▽°*)╯ "," (｡◕ˇ∀ˇ◕）","o(*≧▽≦)ツ","≖‿≖✧",">ㅂ<","ˋ▽ˊ","\\(•ㅂ•)/♥",
+			"✪ε✪","✪υ✪","✪ω✪","눈_눈",",,Ծ‸Ծ,,","π__π","（/TДT)/","ʅ（´◔౪◔）ʃ","(｡☉౪ ⊙｡)","o(*≧▽≦)ツ┏━┓拍桌狂笑",
+			" (●'◡'●)ﾉ♥","<(▰˘◡˘▰)>","｡◕‿◕｡","(｡・`ω´･)","(♥◠‿◠)ﾉ","ʅ(‾◡◝) "," (≖ ‿ ≖)✧","（´∀｀*)",
+			"（＾∀＾）","(o^∇^o)ﾉ","ヾ(=^▽^=)ノ","(*￣∇￣*)"," (*´∇｀*)","(*ﾟ▽ﾟ*)","(｡･ω･)ﾉﾞ","(≡ω≡．)",
+			"(｀･ω･´)","(´･ω･｀)","(●´ω｀●)φ)"}
+		rand.Seed(time.Now().Unix())
+		return happy[rand.Intn(len(happy))]
+}
+
+func (this *ApiServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+  msg := r.PostFormValue("msg")
+
+	uid := r.PostFormValue("uid")
+
+	robotName := r.PostFormValue("robot")
+
+	if uid == ""{
+		uid = "myself"
+	}
+
+	if robotName == ""{
+		fmt.Println("robot名字未定义")
+		return
+	}
+
+	switch this.ApiName {
+	case "message":
+		var reply string
+		fmt.Println(msg)
+		if strings.Contains(msg, "颜文字"){
+			e := randomEmoticon()
+			fmt.Fprint(w, e)
+			return
+		}
+		turingConfig, _ := getConfig("turing")
+		robotConfig,err := getConfig(robotName)
+		if err != nil{
+			fmt.Println("robot配置未定义")
+			return
+		}else if _,exist :=robotConfig["key"]; exist == false{
+			fmt.Println("robot配置未定义")
+			return
+		}
+		resp, err := http.PostForm(turingConfig["base_url"], url.Values{"uid":{uid}, "key": {robotConfig["key"]}, "info": {msg}})
+		if err != nil{
+			fmt.Println("request fail")
+			return
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil{
+			fmt.Println("request fail")
+			return
+		}
+		fmt.Println(string(body))
+		replyMap := JsonDecode(string(body))
+		if replyMap["code"] == 100000{
+			reply = replyMap["text"].(string)
+		}else if replyMap["code"] == 200000{
+			reply = "给你个链接接着  " + replyMap["url"].(string)
+		}else if replyMap["code"] == 40002{
+			reply = "不知道你在说虾米～"
+		}else if replyMap["code"] == 40004{
+			reply = "今天太累了，明天再聊吧"
+		}else{
+			reply = "哦"
+		}
+		fmt.Fprint(w, reply)
+	default:
+		fmt.Fprint(w, "Invalid api")
+	}
+}
--- weixin.go
+++ weixin.go
@@ -6,9 +6,15 @@ import (
   "net/http"
   // "log"
 	"net/url"
-  "io/ioutil"
   "time"
   "regexp"
+  "os"
+  "os/exec"
+  "io/ioutil"
+  "io"
+  "bytes"
+  "strconv"
+  "strings"
   // "strings"
 )
 
@@ -18,22 +24,39 @@ type wxweb struct {
 
 type jsonData map[string]interface{}
 
-func (self *wxweb) getUuid() bool{
+func (self *wxweb) getUuid(args ...interface{}) bool{
   urlstr := "https://login.weixin.qq.com/jslogin"
   params := make(map[string]interface{})
   params["appid"] = "wx782c26e4c19acffb"
 	params["fun"] = "new"
 	params["lang"] = "zh_CN"
-  params["_"] = string(time.Now().Unix())
+  params["_"] = self.unixStr()
   data,_ := self._post(urlstr, params, false)
   re := regexp.MustCompile("\"(\\S+?)\"")
   find := re.FindAllString(data, -1)
   if len(find) > 0{
-    fmt.Println(find)
-    self.uuid = find[0]
+    self.uuid = strings.Replace(find[0],"\"","",-1)
     return true
   }else{
     return false
+  }
+}
+
+func (self *wxweb) _run(desc string, f func(...interface{}) bool, args... interface{}) {
+  fmt.Print(desc)
+  var result bool
+  if len(args) > 1 {
+    result = f(args)
+  } else if len(args) == 1 {
+    result = f(args[0])
+  } else {
+    result = f()
+  }
+  if result{
+    fmt.Println("成功")
+  }else{
+    fmt.Println("失败\n[*] 退出程序");
+    os.Exit(1)
   }
 }
 
@@ -54,4 +77,34 @@ func (self *wxweb) _post(urlstr string, params jsonData, jsonFmt bool) (string, 
     return res,err
   }
   return string(body),nil
+}
+
+func (self *wxweb) unixStr() string{
+  return strconv.Itoa(int(time.Now().Unix()))
+}
+
+//TODO support linux
+func (self *wxweb) genQRcode(args ...interface{}) bool{
+  urlstr := "https://login.weixin.qq.com/qrcode/" + self.uuid
+  urlstr += "?t=webwx"
+  urlstr += "&_=" + self.unixStr()
+  path := "qrcode.jpg"
+  out, err := os.Create(path)
+  defer out.Close()
+  resp, err := http.Get(urlstr)
+  defer resp.Body.Close()
+  pix, err := ioutil.ReadAll(resp.Body)
+  _, err = io.Copy(out, bytes.NewReader(pix))
+  if err != nil{
+    return false
+  }else{
+    exec.Command("open",path).Run()
+    return true
+  }
+}
+
+func (self *wxweb) start(){
+  fmt.Println("[*] 微信网页版 ... 开动")
+  self._run("[*] 正在获取 uuid ... ", self.getUuid)
+  self._run("[*] 正在获取 二维码 ... ", self.genQRcode)
 }
