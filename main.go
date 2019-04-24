b0c9965a776803c02de39288da88754bec3786fb
mike <zhangfan106975@pwrd.com>
2019-04-24 17:30:03 +0800

    'm'

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	// "log"
 	// "net/http"
@@ -18,7 +19,18 @@ func Error(msg string) error {
 	return &ServiceError{msg}
 }
 
+const (
+	Version = "0.1.0"
+)
+
+var debug = flag.String("d", "off", "if on debug mode")
+
 func main() {
+
+	flag.Parse()
+
+	fmt.Printf("debug mode %s\n", *debug)
+
 	wx := wxweb{}
 	wx.start()
 }
